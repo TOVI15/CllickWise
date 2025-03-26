e8ed607060362036558abbd1b853f60cd2ce675f
“TOVI15” <036164702a@gmail.com>
2025-03-27 00:30:49 +0200

    update to cloud

--- react/src/Router.tsx
+++ react/src/Router.tsx
@@ -6,15 +6,21 @@ import AddRecipes from "./components/recipe/AddRecipes";
 import All from "./components/recipe/SeeRecipes";
 import Login from "./components/user/Login";
 import AppLayout from "./components/main/AppLayout";
+import StudentDatabase from "./components/students/StudentDatabase";
+import RegisteredStudents from "./components/students/RegisteredStudents";
+import StudentEditCard from "./components/students/StudentEditCard";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <Login /> }, 
+  { path: "/", element: <Login /> },
 
   {
     path: "/main",
-    element: <AppLayout/>, 
+    element: <AppLayout />,
     children: [
       { path: "about", element: <About /> },
+      { path: "studentDatebase", element: <StudentDatabase /> },
+      { path: "edit-student/:id", element: <StudentEditCard /> },
+      { path: "registedStudent", element: <RegisteredStudents students={[]} registeredStudents={undefined} /> },
       { path: "recipes", element: <All /> },
       { path: "addrecipes", element: <Provider store={store}><AddRecipes /></Provider> },
     ],
---
+++ react/src/moduls/Student.ts
@@ -0,0 +1,10 @@
+export type typeStudent = {
+    id?: number,
+    firstName?: string,
+    lastName?: string,
+    email?: string,
+    password?: string,
+    phone?: string,
+    address?: string,
+    
+  }
